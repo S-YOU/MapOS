16f3c625003a0756ec0521c9bf92c02dac861401
mapfap <mapkabb@gmail.com>
2013-12-20 12:17:28 +0700

    Initial commit

---
+++ cross/lib/gcc/i586-elf/4.8.2/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc-4.8.2/configure --target=i586-elf --prefix=/Users/mapfap/os/cross --disable-nls --enable-languages=c,c++ --without-headers";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "pentium" }, { "arch", "pentium" } };
